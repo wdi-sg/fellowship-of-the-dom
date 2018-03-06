f6e0021be95fa1f621563abde07f012490ff4a33
Edmund <edmundcjj@gmail.com>
2018-03-06 16:35:19 +0800

    Completed till Part 6

--- starter-code/app/scripts/fellowship.js
+++ starter-code/app/scripts/fellowship.js
@@ -19,7 +19,6 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.querySelector('body');
 
-
 // Part 1
 
 function makeMiddleEarth() {
@@ -46,7 +45,6 @@ makeMiddleEarth();
 function makeHobbits() {
   // display an unordered list of hobbits in the shire (which is the second article tag on the page)
   var shire = document.querySelectorAll("article")[0];
-  console.log("Shire is " + shire);
   var hobbit_list = document.createElement("ul");
   for (var i = 0; i < hobbits.length; i++) {
     var li = document.createElement("li");
@@ -78,29 +76,47 @@ keepItSecretKeepItSafe();
 // Part 4
 function makeBuddies() {
   // create an aside tag
+  var aside_tag = document.createElement("aside");
   // attach an unordered list of the 'buddies' in the aside
+  var buddy_list = document.createElement("ul");
+  for (var i = 0; i < buddies.length; i++) {
+    var li = document.createElement("li");
+    li.innerHTML = buddies[i];
+    buddy_list.appendChild(li);
+  }
+  aside_tag.appendChild(buddy_list);
   // insert your aside as a child element of rivendell
+  var rivendell = document.querySelectorAll("article")[1];
+  rivendell.appendChild(aside_tag);
 }
+makeBuddies();
 
 
 // Part 5
-
-
 function beautifulStranger() {
   // change the 'Strider' textnode to 'Aragorn'
+  var rivendell = document.querySelectorAll("li");
+  for (var i = 0; i < rivendell.length; i++) {
+    if (rivendell[i].innerText === "strider") {
+      console.log("Match found");
+      rivendell[i].innerText = "Aragorn";
+    }
+  }
 }
+beautifulStranger();
 
 
 // Part 6
-
 function leaveTheShire() {
   // assemble the hobbits and move them to Rivendell
+  var hobbits = document.querySelectorAll("ul")[0];
+  var rivendell = document.querySelectorAll("ul")[1];
+  rivendell.appendChild(hobbits);
 }
+leaveTheShire();
 
 
 // Part 7
-
-
 function forgeTheFellowShip() {
   // create a new div called 'the-fellowship' within rivendell
   // add each hobbit and buddy one at a time to 'the-fellowship'
@@ -109,8 +125,6 @@ function forgeTheFellowShip() {
 
 
 // Part 8
-
-
 function theBalrog() {
   // change the 'Gandalf' textNode to 'Gandalf the White'
   // apply style to the element
@@ -119,7 +133,6 @@ function theBalrog() {
 
 
 // Part 9
-
 function hornOfGondor() {
   // pop up an alert that the horn of gondor has been blown
   // Boromir's been killed by the Uruk-hai!
@@ -129,7 +142,6 @@ function hornOfGondor() {
 
 
 // Part 10
-
 function itsDangerousToGoAlone(){
   // take Frodo and Sam out of the fellowship and move them to Mordor
   // add a div with an id of 'mount-doom' to Mordor
@@ -137,7 +149,6 @@ function itsDangerousToGoAlone(){
 
 
 // Part 11
-
 function weWantsIt() {
   // Create a div with an id of 'gollum' and add it to Mordor
   // Remove the ring from Frodo and give it to Gollum
@@ -146,7 +157,6 @@ function weWantsIt() {
 
 
 // Part 12
-
 function thereAndBackAgain() {
   // remove Gollum and the Ring from the document
   // remove all the baddies from the document
